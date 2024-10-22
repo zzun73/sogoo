4910e1e1adabedb6932c96a2bf12a606c9795401
최승현 <csh7099@naver.com>
2024-10-22 15:20:39 +0900

    feat : 회원탈퇴 여부 추가

--- backend/main/src/main/java/com/ssafy/c107/main/domain/members/entity/Member.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/members/entity/Member.java
@@ -2,6 +2,8 @@ package com.ssafy.c107.main.domain.members.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,6 +43,10 @@ public class Member {
 
     @Column(nullable = false)
     private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WithDrawalStatus withDrawalStatus;
 
     private String role;
 
---
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/members/entity/WithDrawalStatus.java
@@ -0,0 +1,5 @@
+package com.ssafy.c107.main.domain.members.entity;
+
+public enum WithDrawalStatus {
+    ACTIVE, INACTIVE
+}
