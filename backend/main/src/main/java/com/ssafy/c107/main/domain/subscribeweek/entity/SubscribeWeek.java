3e21ac025da46e214607381f04b79c7fba1fdedf
최승현 <csh7099@naver.com>
2024-10-24 15:35:54 +0900

    feat : 구독 주차 엔티티 생성

---
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/subscribeweek/entity/SubscribeWeek.java
@@ -0,0 +1,38 @@
+package com.ssafy.c107.main.domain.subscribeweek.entity;
+
+import com.ssafy.c107.main.common.entity.BaseEntity;
+import com.ssafy.c107.main.domain.subscribe.entity.Subscribe;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "SubscribeWeek")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubscribeWeek extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private int round;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "subscribe_id")
+    private Subscribe subscribe;
+}
