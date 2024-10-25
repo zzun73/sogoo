7555b967291467f6f1d33288041aae401324f6c3
최승현 <csh7099@naver.com>
2024-10-25 09:28:23 +0900

    fix : 멤버 구독 테이블 컬럼 추가(구독 상태, 구독 종료 날짜)

--- backend/main/src/main/java/com/ssafy/c107/main/domain/subscribe/entity/MemberSubscribe.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/subscribe/entity/MemberSubscribe.java
@@ -3,6 +3,8 @@ package com.ssafy.c107.main.domain.subscribe.entity;
 import com.ssafy.c107.main.common.entity.BaseEntity;
 import com.ssafy.c107.main.domain.members.entity.Member;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +26,11 @@ public class MemberSubscribe extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private SubscribeStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "member_id")
