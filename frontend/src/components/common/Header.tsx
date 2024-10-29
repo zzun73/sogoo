ff3ddc2726ed881346222f6b0ab31f23e9cb13d6
서규범 <seo980916@gmail.com>
2024-10-29 17:56:51 +0900

    feat: Header에 Link 추가

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -3,12 +3,22 @@ import LogoImg from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <header className="flex flex-col items-center">
+    <header className="flex flex-col items-center shadow-md">
       <div className="my-10">
         <img className="w-[100px]" src={LogoImg} alt="소상한 구독" />
       </div>
-      <nav>
-        <Link to="/">홈</Link>
+      <nav className="flex flex-row justify-between w-full h-10 px-16">
+        {/* 페이지 이동 */}
+        <div className="flex gap-8">
+          <Link to="/">홈</Link>
+          <Link to="/store">매장 조회</Link>
+        </div>
+        {/* user 관련 */}
+        <div className="flex gap-8">
+          <Link to="/mypage">마이페이지</Link>
+          <Link to="/cart">장바구니</Link>
+          <Link to="/auth">로그인/회원가입</Link>
+        </div>
       </nav>
     </header>
   );
