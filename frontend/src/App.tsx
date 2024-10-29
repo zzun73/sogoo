bc146f4c6fdbac51bd52f3a2c78f7032d68de338
서규범 <seo980916@gmail.com>
2024-10-29 16:13:30 +0900

    fix: react-router-dom 관련 세팅 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import "./index.css"
-import Header from "./components/common/Header"
+import "./index.css";
+import Header from "./components/common/Header";
 
 function App() {
   return (
     <>
       <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
