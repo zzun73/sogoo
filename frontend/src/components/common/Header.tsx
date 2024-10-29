3688344224289d636da8e3394fc86144c1f3fc09
goosebbeoms <seo980916@gmail.com>
2024-10-29 22:19:02 +0900

    feat: Header CSS 수정, 아이콘 추가

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,22 +1,28 @@
 import { Link } from "react-router-dom";
 import LogoImg from "../../assets/logo.png";
+import { MdOutlinePerson } from "react-icons/md";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Header = () => {
   return (
     <header className="flex flex-col items-center shadow-md">
-      <div className="my-10">
-        <img className="w-[100px]" src={LogoImg} alt="소상한 구독" />
+      <div className="mt-10 mb-5">
+        <img src={LogoImg} alt="소상한 구독" className="w-[120px] drop-shadow-lg" />
       </div>
-      <nav className="flex flex-row justify-between w-full h-10 px-16">
+      <nav className="flex flex-row justify-between items-center w-full h-10 px-16">
         {/* 페이지 이동 */}
-        <div className="flex gap-8">
+        <div className="flex items-center gap-8">
           <Link to="/">홈</Link>
           <Link to="/store">매장 조회</Link>
         </div>
         {/* user 관련 */}
-        <div className="flex gap-8">
-          <Link to="/mypage">마이페이지</Link>
-          <Link to="/cart">장바구니</Link>
+        <div className="flex items-center gap-8">
+          <Link to="/mypage">
+            <MdOutlinePerson className="w-5 h-5" />
+          </Link>
+          <Link to="/cart">
+            <MdOutlineShoppingCart className="w-5 h-5" />
+          </Link>
           <Link to="/auth">로그인/회원가입</Link>
         </div>
       </nav>
