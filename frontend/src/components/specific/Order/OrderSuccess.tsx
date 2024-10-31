4907005e3f85485c0f1ebde249af4ac76fbf3f63
서규범 <seo980916@gmail.com>
2024-10-31 17:27:50 +0900

    feat: OrderSuccess 컴포넌트 내 미사용 MUI 태그 임시 주석화

--- frontend/src/components/specific/Order/OrderSuccess.tsx
+++ frontend/src/components/specific/Order/OrderSuccess.tsx
@@ -24,23 +24,12 @@ const OrderSuccess = () => {
             </div>
             <div className="flex items-center">
               <p className="w-32">배송지</p>
-              <div className="flex-1 w-full">
-                <TextField fullWidth id="recipientAddress" placeholder="배송지 주소" onChange={handleRecipientAddressChange} />
-              </div>
+              <div className="flex-1 w-full">{/* <TextField fullWidth id="recipientAddress" placeholder="배송지 주소" onChange={handleRecipientAddressChange} /> */}</div>
             </div>
             <div className="flex items-center">
               <p className="w-32">배송 요청사항</p>
-              <div className="flex-1 w-full">
-                <TextField fullWidth id="request" placeholder="배송 요청사항" onChange={handleRequestChange} />
-              </div>
+              <div className="flex-1 w-full">{/* <TextField fullWidth id="request" placeholder="배송 요청사항" onChange={handleRequestChange} /> */}</div>
             </div>
-          </div>
-        </div>
-        {/* 결제 수단 */}
-        <div className="flex flex-col gap-8 w-full bg-cyan-50 p-8 rounded-xl">
-          <h3 className="text-xl font-semibold">결제 수단</h3>
-          <div>
-            <TossPaymentsCheckout />
           </div>
         </div>
       </div>
