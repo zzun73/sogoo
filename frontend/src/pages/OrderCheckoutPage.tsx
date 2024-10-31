177c72cb1b8aca5220a7fee20b6001787ce64476
서규범 <seo980916@gmail.com>
2024-10-31 10:25:38 +0900

    feat: 결제 페이지 접근 시 로그인 유무 필터링 추가

--- frontend/src/pages/OrderCheckoutPage.tsx
+++ frontend/src/pages/OrderCheckoutPage.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import useRootStore from "../stores";
 
 const OrderCheckoutPage = () => {
+  const navigate = useNavigate();
+  const isLogin = useRootStore((state) => state.isLogin);
+
+  if (!isLogin) {
+    return navigate("/sign");
+  }
+
   return (
     <div className="flex flex-col items-center py-14">
       <Outlet />
