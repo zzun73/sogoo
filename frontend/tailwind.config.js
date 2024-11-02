646844970301b0a2412a1198407f67de422406e4
서규범 <seo980916@gmail.com>
2024-11-03 02:03:34 +0900

    feat: tailwind.config.js 파일 내 color 코드 정의 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,11 @@ export default {
         "paper-pattern": "url('/src/assets/paper1.jpg')",
         "black-paper": "url('/src/assets/black-paper.jpg')",
       },
+      colors: {
+        main: "#6F4F3A", // 메인 색상(Deep Mocha)
+        sub: "#D9BCA5", // 서브 색생(Warm Beidge)
+        point: "#B33D3D", // 포인트 색상(Traditional Red)
+      },
       fontFamily: {
         shilla: ["Shilla_CultureB-Bold"],
         dimibang: ["dimibang_new"],
@@ -14,6 +19,5 @@ export default {
       },
     },
   },
-
   plugins: [],
 };
