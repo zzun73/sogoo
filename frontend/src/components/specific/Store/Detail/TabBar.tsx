dca26de0b8d42ec6a002d28c8b7772deffa3945b
황민채 <trick0846@naver.com>
2024-11-04 05:39:48 +0900

    feat: 매장 상세정보 TabBar 구현

--- frontend/src/components/specific/Store/Detail/TabBar.tsx
+++ frontend/src/components/specific/Store/Detail/TabBar.tsx
@@ -1,5 +1,35 @@
-const TabBar = () => {
-  return <div></div>;
+interface TabProps {
+  key: string;
+  word: string;
+}
+
+interface TabBarProps {
+  selectedTab: string;
+  handleTab: (tab: string) => void;
+}
+
+const TabBar = ({ selectedTab, handleTab }: TabBarProps) => {
+  const tabs: TabProps[] = [
+    { key: "subscribe", word: "구독 상품 목록" },
+    { key: "foods", word: "개별 상품 목록" },
+    { key: "review", word: "리뷰" },
+  ];
+
+  return (
+    <div className="grid grid-cols-3 text-center mt-10">
+      {tabs.map((item) => (
+        <div
+          key={item.key}
+          className={`py-5 ${
+            selectedTab === item.key ? "border border-black border-b-0" : ""
+          }`}
+          onClick={() => handleTab(item.key)}
+        >
+          {item.word}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TabBar;
--- frontend/src/components/specific/Store/StoreDetail.tsx
+++ frontend/src/components/specific/Store/StoreDetail.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 import StoreInfo from "./Detail/StoreInfo";
 import StoreContent from "./Detail/StoreContent";
 import TabBar from "./Detail/TabBar";
 
 const StoreDetail = () => {
+  const [selectedTab, setSelectedTab] = useState("subscribe");
+  const handleTab = (key: string) => {
+    setSelectedTab(key);
+  };
   return (
     <div>
       <StoreInfo />
-      <TabBar />
+      <TabBar selectedTab={selectedTab} handleTab={handleTab} />
       <StoreContent />
     </div>
   );
