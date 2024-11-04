081d5999afc3464e537c6873a75916250fd60350
최승현 <csh7099@naver.com>
2024-11-04 13:40:30 +0900

    feat : 가게 등록 controller 생성

--- backend/main/src/main/java/com/ssafy/c107/main/domain/store/controller/StoreController.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/store/controller/StoreController.java
@@ -1,11 +1,15 @@
 package com.ssafy.c107.main.domain.store.controller;
 
+import com.ssafy.c107.main.domain.members.dto.CustomUserDetails;
+import com.ssafy.c107.main.domain.members.exception.InvalidMemberRoleException;
 import com.ssafy.c107.main.domain.store.dto.GetStoreDto;
+import com.ssafy.c107.main.domain.store.dto.request.AddStoreRequest;
 import com.ssafy.c107.main.domain.store.dto.response.GetStoreResponse;
 import com.ssafy.c107.main.domain.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -27,5 +31,15 @@ public class StoreController {
     public ResponseEntity<?> getStore(@PathVariable Long storeId) {
         GetStoreDto getStoreDto = storeService.getStoreById(storeId);
         return ResponseEntity.ok(getStoreDto);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addStore(@ModelAttribute AddStoreRequest addStoreRequest,
+        @AuthenticationPrincipal
+        CustomUserDetails customUserDetails) {
+        if (customUserDetails.getUserRole().getRole().equals("Buyer")) {
+            throw new InvalidMemberRoleException();
+        }
+        return null;
     }
 }
