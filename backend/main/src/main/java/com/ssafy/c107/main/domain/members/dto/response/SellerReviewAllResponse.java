2d46198272ee56da438b4a9734147bfc19310fdd
최승현 <csh7099@naver.com>
2024-11-04 09:58:42 +0900

    feat : 판매자 전체 리뷰 api dto 개발

---
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/members/dto/response/SellerReviewAllResponse.java
@@ -0,0 +1,13 @@
+package com.ssafy.c107.main.domain.members.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SellerReviewAllResponse {
+
+    private int storeId;
+    private int positiveCnt;
+    private int negativeCnt;
+}
