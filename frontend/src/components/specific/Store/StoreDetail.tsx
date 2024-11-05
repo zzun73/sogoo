d4a52d8b9b0f728d466d6bbc25ed2aa99c45dc5a
황민채 <trick0846@naver.com>
2024-11-05 11:06:38 +0900

    fix: storeDetail 컴포넌트 수정

--- frontend/src/components/specific/Store/StoreDetail.tsx
+++ frontend/src/components/specific/Store/StoreDetail.tsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
-
 import StoreInfo from "./Detail/StoreInfo";
 import StoreContent from "./Detail/StoreContent";
-import TabBar from "./Detail/TabBar";
-
 const StoreDetail = () => {
-  const [selectedTab, setSelectedTab] = useState("subscribe");
-  const handleTab = (key: string) => {
-    setSelectedTab(key);
-  };
   return (
     <div>
       <StoreInfo />
-      <TabBar selectedTab={selectedTab} handleTab={handleTab} />
-      <StoreContent selectedTab={selectedTab} />
+      <StoreContent />
     </div>
   );
 };
