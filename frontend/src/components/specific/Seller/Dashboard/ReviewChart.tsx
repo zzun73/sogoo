65333b8f325d7ba45ca19eda407d204b8a0679f9
황민채 <trick0846@naver.com>
2024-11-06 07:48:27 +0900

    feat: ReviewChart 생성

---
+++ frontend/src/components/specific/Seller/Dashboard/ReviewChart.tsx
@@ -0,0 +1,11 @@
+import Box from "../../../common/Box";
+
+const ReviewChart = () => {
+  return (
+    <Box className="flex flex-col justify-center w-full">
+      <p>ReviewChart</p>
+    </Box>
+  );
+};
+
+export default ReviewChart;
