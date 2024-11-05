5d7d14acc6105cf8bea30f265b0bea9932bfaedb
박동민 <pmin1616@naver.com>
2024-11-05 12:13:46 +0900

    feat, design: padding을 ChoiceStore가 아닌 Dashboard 자체에 적용, 선택한 가게의 storeId를 메뉴 목록 페이지로 전달

--- frontend/src/components/specific/Seller/Dashboard.tsx
+++ frontend/src/components/specific/Seller/Dashboard.tsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ChoiceStore from "./MenuComponents/ChoiceStore";
+import Button from "@mui/material/Button";
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
   const [selectedStoreId, setSelectedStoreId] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const handleStoreSelect = (storeId: string | null) => {
     setSelectedStoreId(storeId);
     console.log("선택된 Store ID:", storeId);
   };
 
+  const handleNavigate = () => {
+    navigate("/seller/menus", { state: { storeId: selectedStoreId } });
+  };
+
   return (
-    <div>
-      <h1>대시보드</h1>
+    <div className="w-full p-5">
       <ChoiceStore onStoreSelect={handleStoreSelect} />
-      {selectedStoreId && <p>현재 선택된 가게 ID: {selectedStoreId}</p>}
+      {selectedStoreId && (
+        <p className="mb-5">현재 선택된 가게 ID: {selectedStoreId}</p>
+      )}
+      <Button onClick={handleNavigate} variant="contained" size="large">
+        판매상품 관리
+      </Button>
     </div>
   );
 };
