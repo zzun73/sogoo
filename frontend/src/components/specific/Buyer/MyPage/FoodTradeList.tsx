42e851c33fa6eb8f7381f383b6b392c510c2a821
서규범 <seo980916@gmail.com>
2024-11-05 11:46:15 +0900

    feat: BuyerMyPage 페이지 컴포넌트 리팩토링(FoodTradeList 컴포넌트 분할)

---
+++ frontend/src/components/specific/Buyer/MyPage/FoodTradeList.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+
+interface FoodTradeListProps {
+  foodTrades: FoodTradeItem[];
+}
+
+const FoodTradeList = ({ foodTrades }: FoodTradeListProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-8 w-full p-8 rounded-t-3xl bg-white">
+        <h3 className="text-xl font-semibold">반찬 주문</h3>
+      </div>
+      <div className="flex flex-col gap-8 w-full p-8 rounded-b-3xl bg-white">
+        <div className="max-h-[600px] p-1 flex flex-col gap-y-2 overflow-y-auto">
+          {foodTrades.map((item) => (
+            <Card
+              key={item.foodId}
+              sx={{
+                width: "100%",
+                minHeight: "150px",
+                boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+              }}
+            >
+              <CardActionArea sx={{ display: "flex" }}>
+                <CardMedia component="img" image={item.foodImg} alt={item.foodName} sx={{ width: "180px", height: "150px", objectFit: "cover" }} />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.foodName}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                    매장명 | {item.storeName}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                    주문상태 | {item.orderStatus}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                    결제 금액 | {item.price}원
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodTradeList;
