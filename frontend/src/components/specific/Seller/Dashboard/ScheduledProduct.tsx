72006de665bed93175b2fe6300ebda6dd2f3429a
황민채 <trick0846@naver.com>
2024-11-06 14:49:29 +0900

    fix: 다음주 출고 예정 상품 테이블 수정

--- frontend/src/components/specific/Seller/Dashboard/ScheduledProduct.tsx
+++ frontend/src/components/specific/Seller/Dashboard/ScheduledProduct.tsx
@@ -4,87 +4,50 @@ import Box from "../../../common/Box";
 const ScheduledProducts = () => {
   const data = {
     columns: [
-      { field: "id", headerName: "순위", width: 20, maxWidth: 30 },
-      {
-        field: "product",
-        headerName: "상품",
-        width: 200,
-        maxWidth: 250,
-        editable: false,
-      },
-      {
-        field: "quantity",
-        headerName: "수량 (단위 : 1개)",
-        width: 200,
-        maxWidth: 250,
-
-        editable: false,
-      },
+      { field: "id", headerName: "순위" },
+      { field: "product", headerName: "상품" },
+      { field: "quantity", headerName: "수량" },
     ],
     rows: [
-      {
-        id: 1,
-        product: "Gold",
-        quantity: 50,
-      },
-      {
-        id: 2,
-        product: "Silver",
-        quantity: 200,
-      },
-      {
-        id: 3,
-        product: "Crude Oil",
-        quantity: 500,
-      },
-      {
-        id: 4,
-        product: "Copper",
-        quantity: 300,
-      },
-      {
-        id: 5,
-        product: "Natural Gas",
-        quantity: 150,
-      },
-      {
-        id: 6,
-        product: "Platinum",
-        quantity: 100,
-      },
-      {
-        id: 7,
-        product: "Palladium",
-        quantity: 25,
-      },
-      {
-        id: 8,
-        product: "Corn",
-        quantity: 800,
-      },
-      {
-        id: 9,
-        product: "Soybeans",
-        quantity: 600,
-      },
-      {
-        id: 10,
-        product: "Wheat",
-        quantity: 400,
-      },
+      { id: 1, product: "Gold", quantity: 50 },
+      { id: 2, product: "Silver", quantity: 200 },
+      { id: 3, product: "Crude Oil", quantity: 500 },
+      { id: 4, product: "Copper", quantity: 300 },
+      { id: 5, product: "Natural Gas", quantity: 150 },
+      { id: 6, product: "Platinum", quantity: 100 },
+      { id: 7, product: "Palladium", quantity: 25 },
+      { id: 8, product: "Corn", quantity: 800 },
+      { id: 9, product: "Soybeans", quantity: 600 },
+      { id: 10, product: "Wheat", quantity: 400 },
     ],
   };
 
   return (
-    <Box className="w-full h-full flex flex-col justify-end">
+    <Box className="w-full h-[300px] flex flex-col justify-between">
       <p className="text-xl text-center font-bold">다음주 출고 예정 상품</p>
-      <DataGridPro
-        {...data}
-        loading={data.rows.length === 0}
-        rowHeight={30}
-        disableColumnPinning
-        className="w-full"
-      />
+      <div className="relative max-h-[230px] overflow-y-auto w-full">
+        <table className="w-full" border={1} cellPadding="5" cellSpacing="0">
+          <thead className="sticky top-0 bg-gray-50">
+            <tr>
+              {data.columns.map((column) => (
+                <th key={column.field} className="text-base">
+                  {column.headerName}{" "}
+                  {column.field === "quentity" && <span></span>}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.rows.map((row) => (
+              <tr key={row.id} className="text-center">
+                <td className="text-sm text-center">{row.id}</td>
+                <td className="text-sm text-center">{row.product}</td>
+                <td className="text-sm text-center">{row.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Box>
   );
 };
