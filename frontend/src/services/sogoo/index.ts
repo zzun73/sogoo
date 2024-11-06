81755963dce75cd1cee8a49eb1225ccf3f11db64
서규범 <seo980916@gmail.com>
2024-11-07 00:59:28 +0900

    feat: 서비스 index.ts에 payment.ts 추가

--- frontend/src/services/sogoo/index.ts
+++ frontend/src/services/sogoo/index.ts
@@ -2,10 +2,12 @@ import auth from "./auth";
 import member from "./member";
 import store from "./store";
 import food from "./food";
+import payment from "./payment";
 
 export default {
   ...auth,
   ...store,
   ...member,
   ...food,
+  ...payment,
 };
