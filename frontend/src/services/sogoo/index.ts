7a42ba97a7ce15055af9288e34ba62b474c97352
황민채 <trick0846@naver.com>
2024-11-07 20:09:22 +0900

    fix: service 도메인별 이동

--- frontend/src/services/sogoo/index.ts
+++ frontend/src/services/sogoo/index.ts
@@ -1,17 +1,13 @@
-import auth from "./auth";
 import member from "./member";
 import store from "./store";
 import food from "./food";
 import payment from "./payment";
-import seller from "./seller";
 import subscribe from "./subscribe";
 
 export default {
-  ...auth,
   ...store,
   ...member,
   ...food,
   ...payment,
-  ...seller,
   ...subscribe,
 };
