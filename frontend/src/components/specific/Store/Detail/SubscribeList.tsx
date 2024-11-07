df042ad4a239f59e927f12efce178ab3412c5828
황민채 <trick0846@naver.com>
2024-11-08 08:27:50 +0900

    feat: 매장 상세 구독상품 skeleton UI 적용

--- frontend/src/components/specific/Store/Detail/SubscribeList.tsx
+++ frontend/src/components/specific/Store/Detail/SubscribeList.tsx
@@ -1,7 +1,41 @@
 import { subscribeData } from "../../../../assets/dummyData";
 import SubscribeInfo from "./SubscribeInfo";
+import { useGetStoreSubscribe } from "../../../../queries/queries";
+import { useParams } from "react-router-dom";
+import { Skeleton } from "@mui/material";
+
+const PackageSkeleton = () => {
+  return (
+    <div className="space-y-4 my-5">
+      <div className="border rounded-lg shadow-sm p-4">
+        <Skeleton variant="text" width="30%" height={30} />
+        <Skeleton variant="text" width="90%" height={20} />
+        <Skeleton variant="text" width="85%" height={20} />
+        <Skeleton variant="text" width="70%" height={20} />
+        <div className="flex justify-end mt-4">
+          <Skeleton variant="circular" width={24} height={24} />
+        </div>
+      </div>
+      <div className="border rounded-lg shadow-sm p-4">
+        <Skeleton variant="text" width="30%" height={30} />
+        <Skeleton variant="text" width="90%" height={20} />
+        <Skeleton variant="text" width="85%" height={20} />
+        <Skeleton variant="text" width="70%" height={20} />
+        <div className="flex justify-end mt-4">
+          <Skeleton variant="circular" width={24} height={24} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const SubscribeList = () => {
+  const { id } = useParams();
+  const subscribes = useGetStoreSubscribe(id);
+  console.log(subscribes);
+  if (!subscribes) {
+    return <PackageSkeleton />;
+  }
   return (
     <div className="flex flex-col gap-y-5 min-h-80 w-11/12 mx-auto mt-5">
       {subscribeData &&
