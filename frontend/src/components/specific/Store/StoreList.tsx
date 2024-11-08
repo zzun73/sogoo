163520a9eb2563ef26fad462d860d56deff27c07
황민채 <trick0846@naver.com>
2024-11-08 15:16:38 +0900

    fix: 매장 전체 조회 css 수정 및 API 연결

--- frontend/src/components/specific/Store/StoreList.tsx
+++ frontend/src/components/specific/Store/StoreList.tsx
@@ -2,9 +2,9 @@ import StoreCard from "./StoreCard";
 import { useGetStoreList } from "../../../queries/queries";
 
 const StoreList = () => {
-  const { stores } = useGetStoreList();
+  const stores = useGetStoreList();
 
-  if (stores) {
+  if (!stores) {
     return (
       <div className="flex w-full h-full justify-center items-center">
         <p>등록된 가게가 없습니다.</p>
@@ -12,60 +12,9 @@ const StoreList = () => {
     );
   }
 
-  const dummy = [
-    {
-      storeId: 1,
-      name: "혁준이네 반찬가게",
-      description: "ㄹㅇ밥도둑",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 2,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 3,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 4,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 5,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 6,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 7,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-    {
-      storeId: 8,
-      name: "승현이네 반찬가게",
-      description: "고기반찬 ~ 고기반찬 ~ 고기반찬이 너무 좋아 ~ !",
-      img: "https://cdn.pixabay.com/photo/2019/01/25/21/35/restaurant-3955372_1280.jpg",
-    },
-  ];
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8">
-      {dummy.map((store) => (
+      {stores.map((store) => (
         <StoreCard store={store} key={`store-${store.storeId}`} />
       ))}
     </div>
