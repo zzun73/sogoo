3527808bff6008768a065ebeb2db49319e4a7ed2
박동민 <pmin1616@naver.com>
2024-11-08 17:56:09 +0900

    feat: 판매자가 선택한 가게 정보 저장하기 위해 localstorage에 selectedStoreId 추가

---
+++ frontend/src/stores/storeSlice.ts
@@ -0,0 +1,10 @@
+import type { StateCreator } from "zustand";
+
+export const createStoreSlice: StateCreator<RootState, [], [], StoreStore> = (
+  set
+) => ({
+  selectedStoreId: null,
+  setSelectedStoreId: (selectedStoreId: SelectedStoreId) => {
+    set({ selectedStoreId });
+  },
+});
