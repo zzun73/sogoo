b31cf73cd6d359d6fd5d29f52cb811d05ac45310
zzun73 <736300@naver.com>
2024-11-08 15:20:09 +0900

    feat: billing key 발급 및 첫 구독 결제 통합

--- backend/main/src/main/java/com/ssafy/c107/main/domain/pay/controller/TossPaymentsController.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/pay/controller/TossPaymentsController.java
@@ -3,8 +3,7 @@ package com.ssafy.c107.main.domain.pay.controller;
 import com.ssafy.c107.main.domain.members.dto.CustomUserDetails;
 import com.ssafy.c107.main.domain.members.exception.InvalidMemberRoleException;
 import com.ssafy.c107.main.domain.pay.service.TossPaymentsServiceImpl;
-import com.ssafy.c107.main.domain.pay.dto.BillingChargeDto;
-import com.ssafy.c107.main.domain.pay.dto.BillingDto;
+import com.ssafy.c107.main.domain.pay.dto.request.AutoBillingRequest;
 import com.ssafy.c107.main.domain.pay.dto.PayDto;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -30,23 +29,27 @@ public class TossPaymentsController {
         return ResponseEntity.ok("결제 승인이 완료되었습니다.");
     }
 
-    @PostMapping("/billing/prepare")
-    public ResponseEntity<String> prepareBillingAuth(@RequestBody BillingDto billingDto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    /**
+     * 구독 자동결제 - tosspayment
+     * 구독 빌링키 발급 및 구독 첫 결제 수행
+     */
+    @PostMapping("/subscriptions")
+    public ResponseEntity<String> prepareBillingAuth(@RequestBody AutoBillingRequest autoBillingRequest, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         validateMemberRoleBuyer(customUserDetails);
 
-        String result = tossPaymentsServiceImpl.prepareBillingAuth(billingDto);
+        String result = tossPaymentsServiceImpl.prepareBillingAuth(customUserDetails.getUserId(), autoBillingRequest);
         log.info("카드 등록: {}", result);
         return ResponseEntity.ok("카드 등록이 완료되었습니다.");
     }
 
-    @PostMapping("/billing/charge")
-    public ResponseEntity<String> chargeBilling(@RequestBody BillingChargeDto billingChargeDto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        validateMemberRoleBuyer(customUserDetails);
-
-        String result = tossPaymentsServiceImpl.chargeBilling(billingChargeDto);
-        log.info("자동 결제: {}", result);
-        return ResponseEntity.ok("자동 결제가 완료되었습니다.");
-    }
+//    @PostMapping("/billing/charge")
+//    public ResponseEntity<String> chargeBilling(@RequestBody BillingChargeDto billingChargeDto, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+//        validateMemberRoleBuyer(customUserDetails);
+//
+//        String result = tossPaymentsServiceImpl.chargeBilling(customUserDetails.getUserId(), billingChargeDto);
+//        log.info("자동 결제: {}", result);
+//        return ResponseEntity.ok("자동 결제가 완료되었습니다.");
+//    }
 
     private static void validateMemberRoleBuyer(CustomUserDetails customUserDetails) {
         if (!customUserDetails.getUserRole().getRole().equals("BUYER")) {
