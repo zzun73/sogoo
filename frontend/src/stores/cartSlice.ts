94ee6a988a6d5d92172fdefce46a00b4d413f035
황민채 <trick0846@naver.com>
2024-11-09 03:43:15 +0900

    fix: 장바구니 초기화 시 storeId 초기화

--- frontend/src/stores/cartSlice.ts
+++ frontend/src/stores/cartSlice.ts
@@ -14,7 +14,15 @@ export const createCartSlice: StateCreator<RootState, [], [], CartStore> = (
 
   // 구독 정보 설정
   setSubscribe: (subInfo) => {
-    set({ subscribe: subInfo });
+    set((state) => {
+      const newState = { ...state, subscribe: subInfo };
+
+      // subscribe와 foodList가 동시에 null인 경우 storeId를 null로 설정
+      if (newState.subscribe === null && (state.foodList?.length ?? 0) === 0) {
+        newState.storeId = null;
+      }
+      return newState;
+    });
   },
 
   // 장바구니 목록 설정
@@ -40,26 +48,50 @@ export const createCartSlice: StateCreator<RootState, [], [], CartStore> = (
 
   // 구매 완료한 제품 삭제
   deleteSelectedList: (selectedIds: number[]) => {
-    set((state) => ({
-      foodList: state.foodList?.filter(
-        (item) => !selectedIds.includes(item.id)
-      ),
-    }));
+    set((state) => {
+      const updatedFoodList =
+        state.foodList?.filter((item) => !selectedIds.includes(item.id)) ?? [];
+
+      const newState = { ...state, foodList: updatedFoodList };
+
+      // subscribe와 foodList가 동시에 null인 경우 storeId를 null로 설정
+      if (state.subscribe === null && updatedFoodList.length === 0) {
+        newState.storeId = null;
+      }
+
+      return newState;
+    });
   },
 
   // 개별 상품 삭제
   deleteSelectedItem: (foodId: FoodId) => {
-    set((state) => ({
-      foodList: state.foodList?.filter((item) => item.id !== foodId),
-    }));
+    set((state) => {
+      const updatedFoodList =
+        state.foodList?.filter((item) => item.id !== foodId) ?? [];
+
+      const newState = {
+        ...state,
+        foodList: updatedFoodList.length ? updatedFoodList : null,
+      };
+
+      // subscribe와 foodList가 동시에 null인 경우 storeId를 null로 설정
+      if (updatedFoodList.length === 0 && state.subscribe === null) {
+        newState.storeId = null;
+      }
+
+      return newState;
+    });
   },
 
   // 수량 변경
   changeFoodCount: (foodId: FoodId, amount: number) => {
-    set((state) => ({
-      foodList: state.foodList?.map((item) =>
-        item.id === foodId ? { ...item, count: item.count + amount } : item
-      ),
-    }));
+    set((state) => {
+      const updatedFoodList =
+        state.foodList?.map((item) =>
+          item.id === foodId ? { ...item, count: item.count + amount } : item
+        ) ?? [];
+
+      return { foodList: updatedFoodList };
+    });
   },
 });
