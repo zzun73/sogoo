6aab0b9b5304ec7ace575166db2c5456654c2098
이학현 <gkrgus008@nate.com>
2024-11-08 11:35:33 +0900

    fix: 반찬가게 상세페이지(구독 상품 목록 조회) 정렬 수정

--- backend/main/src/main/java/com/ssafy/c107/main/domain/subscribe/repository/SubscribeRepository.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/subscribe/repository/SubscribeRepository.java
@@ -14,7 +14,8 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
             "JOIN FETCH s.subscribeWeeks sw " +   // Subscribe와 SubscribeWeek를 조인
             "JOIN FETCH sw.weeklyFoods wf " +     // SubscribeWeek와 WeeklyFood를 조인
             "JOIN FETCH wf.food f " +             // WeeklyFood와 Food를 조인
-            "WHERE st.id = :storeId "
+            "WHERE st.id = :storeId " +
+            "order by sw.round"
     )
     Optional<Subscribe> findSubscribeWithDetailsByStoreId(Long storeId);
 
