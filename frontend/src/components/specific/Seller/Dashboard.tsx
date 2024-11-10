26045f9d775f04a77f46363e2b434845b986170b
황민채 <trick0846@naver.com>
2024-11-11 04:07:20 +0900

    fix: 대시보드 css 수정

--- frontend/src/components/specific/Seller/Dashboard.tsx
+++ frontend/src/components/specific/Seller/Dashboard.tsx
@@ -8,17 +8,17 @@ const Dashboard: React.FC = () => {
   const { selectedStoreId } = useRootStore();
   if (!selectedStoreId) {
     return (
-      <div className="flex justify-center items-center bg-white rounded-3xl h-[500px] mx-[200px] my-5">
+      <div className="flex justify-center items-center bg-white rounded-3xl h-[500px] mx-[200px] my-5 min-w-[1100px]">
         <p className="text-sub font-bold">가게를 선택해주세요.</p>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col gap-y-5 w-full px-[200px] md-5">
+    <div className="flex flex-col gap-y-5 mx-[200px] min-w-[1100px] md-5">
       <SalesOverview storeId={selectedStoreId} />
       <div className="grid grid-cols-2 gap-x-3 w-full">
-        <MonthlySales />
+        <MonthlySales storeId={selectedStoreId} />
         <ScheduledProducts />
       </div>
       <div className="grid grid-cols-2 gap-x-3 w-full">
