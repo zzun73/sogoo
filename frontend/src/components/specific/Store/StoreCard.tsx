e7c819314e7473dd03e6008adbb26e7e85f07368
서규범 <seo980916@gmail.com>
2024-11-11 17:50:20 +0900

    feat: 매장 조회 카드 배치

--- frontend/src/components/specific/Store/StoreCard.tsx
+++ frontend/src/components/specific/Store/StoreCard.tsx
@@ -11,12 +11,12 @@ const StoreCard = ({ store }: StoreProps) => {
     <Link to={`/store/${storeId}`}>
       <Card className="hover:scale-105">
         <CardMedia component="img" image={img} alt={name} sx={{ width: "100%", aspectRatio: "1 / 1", objectFit: "cover" }} />
-        <div className="flex flex-row justify-between px-4 items-center py-2">
+        <div className="flex flex-row justify-start px-4 items-center py-2">
           <p className="text-lg font-bold truncate">{name}</p>
         </div>
         <CardContent sx={{ padding: "10px" }}>
           <Typography variant="body2" sx={{ color: "text.secondary", minHeight: "30px" }}>
-            <p className="truncate text-base h-10 px-1">{description}</p>
+            <p className="truncate text-base px-1">{description}</p>
           </Typography>
         </CardContent>
       </Card>
