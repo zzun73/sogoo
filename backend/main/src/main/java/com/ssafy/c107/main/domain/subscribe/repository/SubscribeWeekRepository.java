425d171a0053d06d6cca019b2727c41234f48d30
zzun73 <736300@naver.com>
2024-11-12 06:45:51 +0900

    feat: 날짜기반 구독 주차 목록 조회 쿼리 추가

--- backend/main/src/main/java/com/ssafy/c107/main/domain/subscribe/repository/SubscribeWeekRepository.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/subscribe/repository/SubscribeWeekRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.c107.main.domain.subscribe.repository;
 
 import com.ssafy.c107.main.domain.subscribe.entity.SubscribeWeek;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +19,10 @@ public interface SubscribeWeekRepository extends JpaRepository<SubscribeWeek, Lo
         + "JOIN FETCH wf.food f "
         + "WHERE sw.id = :swId")
     Optional<SubscribeWeek> findBySwId(@Param("swId") Long swId);
+
+    @Query("SELECT sw FROM SubscribeWeek sw " +
+            "JOIN FETCH sw.subscribe s " +
+            "WHERE s.id = :subscribeId AND sw.startDate = :startDate")
+    List<SubscribeWeek> findBySubscribeIdAndStartDate(@Param("subscribeId") Long subscribeId,
+                                                      @Param("startDate") LocalDate startDate);
 }
