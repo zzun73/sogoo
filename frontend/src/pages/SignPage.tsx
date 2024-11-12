1de59e2f06a09fdea48ae04b737649390317b1c9
서규범 <seo980916@gmail.com>
2024-11-12 09:41:19 +0900

    feat: 로그인/회원가입 페이지 로고 hover 효과 수정

--- frontend/src/pages/SignPage.tsx
+++ frontend/src/pages/SignPage.tsx
@@ -16,7 +16,7 @@ const SignPage = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <BrushBorder className="h-full max-h-[500px] flex items-center pr-20" borderColor="#333333" strokeWidth={4}>
-        <img className="w-60 mb-10 cursor-pointer animate-pulse" src={LogoImg} alt="소상한 구독" onClick={goToLanding} />
+        <img className="w-60 mb-10 cursor-pointer transform duration-300 hover:scale-[1.05]" src={LogoImg} alt="소상한 구독" onClick={goToLanding} />
       </BrushBorder>
 
       <div className="w-20"></div>
@@ -24,13 +24,17 @@ const SignPage = () => {
         <div className="flex w-full justify-center mb-10">
           <div
             onClick={() => setClickedLogin(true)}
-            className={`w-1/2 h-10 border-2 rounded flex justify-center items-center ${clickedLogin ? "border-blue-500" : "border-inherit"} cursor-pointer`}
+            className={`w-1/2 h-10 border-2 rounded flex justify-center items-center ${
+              clickedLogin ? "border-blue-500" : "border-inherit"
+            } cursor-pointer`}
           >
             로그인
           </div>
           <div
             onClick={() => setClickedLogin(false)}
-            className={`w-1/2 h-10 border-2 rounded flex justify-center items-center ${!clickedLogin ? "border-blue-500" : "border-inherit"} cursor-pointer`}
+            className={`w-1/2 h-10 border-2 rounded flex justify-center items-center ${
+              !clickedLogin ? "border-blue-500" : "border-inherit"
+            } cursor-pointer`}
           >
             회원가입
           </div>
