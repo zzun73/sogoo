8e125576f867a5d1250ca970b99c3038570914de
이학현 <gkrgus008@nate.com>
2024-11-12 15:11:56 +0900

    fix: 판매자 마이페이지(다음주 출고량) 수정

--- backend/main/src/main/java/com/ssafy/c107/main/domain/members/service/SellerServiceImpl.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/members/service/SellerServiceImpl.java
@@ -38,6 +38,7 @@ import com.ssafy.c107.main.domain.subscribe.repository.MemberSubscribeRepository
 import com.ssafy.c107.main.domain.subscribe.repository.SubscribePayRepository;
 import com.ssafy.c107.main.domain.subscribe.repository.SubscribeRepository;
 import com.ssafy.c107.main.domain.subscribe.repository.SubscribeWeekRepository;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -52,6 +53,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -93,15 +95,15 @@ public class SellerServiceImpl implements SellerService {
 
         //구독자 수 가져오기
         List<SubscribeStatus> statuses = Arrays.asList(SubscribeStatus.SUBSCRIBE,
-            SubscribeStatus.CANCEL_SCHEDULE);
+                SubscribeStatus.CANCEL_SCHEDULE);
         int subscribeCount = memberSubscribeRepository.getSubscribeMembers(storeId, statuses).intValue();
 
         return SalesStatusResponse
-            .builder()
-            .todaySales(todayOrderPrice + todaySubscribePrice)
-            .todayTradeCnt(todayOrderCount)
-            .subscribePeopleCnt(subscribeCount)
-            .build();
+                .builder()
+                .todaySales(todayOrderPrice + todaySubscribePrice)
+                .todayTradeCnt(todayOrderCount)
+                .subscribePeopleCnt(subscribeCount)
+                .build();
     }
 
     @Override
@@ -109,14 +111,14 @@ public class SellerServiceImpl implements SellerService {
         memberValidator.validStoreAndMember(storeId, userId);
 
         List<String> monthsData = List.of("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
-            "Sep", "Oct", "Nov", "Dec");
+                "Sep", "Oct", "Nov", "Dec");
 
         // 1. 해당 가게의 월별 금액 가져오기
         // 1-1. 개별 반찬 금액 가져오기
         LocalDateTime startDate = LocalDateTime.now().minusYears(1);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM", Locale.ENGLISH);
         List<Object[]> monthlySumForLastYear = orderRepository.findMonthlySumForLastYear(startDate,
-            storeId);
+                storeId);
 
         Map<String, Long> foodMap = new HashMap<>();
 
@@ -130,7 +132,7 @@ public class SellerServiceImpl implements SellerService {
 
         // 1-2. 구독 금액 가져오기
         List<Object[]> monthlyRevenueByStoreId = subscribePayRepository.findMonthlyRevenueByStoreId(
-            storeId);
+                storeId);
 
         Map<String, Long> subscribeMap = new HashMap<>();
 
@@ -151,10 +153,10 @@ public class SellerServiceImpl implements SellerService {
         }
 
         return MonthlySalesResponse
-            .builder()
-            .foodSales(foodMap)
-            .subscribeSales(subscribeMap)
-            .build();
+                .builder()
+                .foodSales(foodMap)
+                .subscribeSales(subscribeMap)
+                .build();
     }
 
     @Override
@@ -170,10 +172,10 @@ public class SellerServiceImpl implements SellerService {
         //구독 상품별 개수 구하기
         Map<Long, Integer> subscribeMap = new TreeMap<>((o1, o2) -> o2.compareTo(o1));
         List<SubscribeStatus> statuses = Arrays.asList(SubscribeStatus.SUBSCRIBE,
-            SubscribeStatus.CANCEL_SCHEDULE);
+                SubscribeStatus.CANCEL_SCHEDULE);
         for (Subscribe subscribe : subscribes) {
             int cnt = memberSubscribeRepository.getCountSubscribes(subscribe.getId(),
-                statuses).intValue();
+                    statuses).intValue();
             if (cnt == 0) {
                 continue;
             }
@@ -185,27 +187,31 @@ public class SellerServiceImpl implements SellerService {
             LocalDate nextMonday = getnextMonday();
 
             //다음주의 구독주차 가져오기
-            SubscribeWeek nextWeek = subscribeWeekRepository.findNextWeek(subscribeId, nextMonday);
+            List<SubscribeWeek> nextWeek = subscribeWeekRepository.findNextWeek(subscribeId, nextMonday);
 
-            SubscribeWeek subscribeWeek = subscribeWeekRepository.findBySwId(nextWeek.getId())
-                .orElseThrow(SubscribeWeekNotFoundException::new);
+            if (nextWeek.isEmpty()) {
+                throw new SubscribeWeekNotFoundException();
+            }
 
-            foodCnt.addAll(subscribeWeek.getWeeklyFoods()
-                .stream()
-                .map(WeeklyFood::getFood)
-                .map(food -> NextWeekFood.builder()
-                    .foodId(food.getId())
-                    .foodName(food.getName())
-                    .foodCnt(subscribeMap.get(subscribeId))
-                    .build())
-                .toList());
+            // 모든 구독 주차에 대해 필요한 반찬 계산
+            for (SubscribeWeek subscribeWeek : nextWeek) {
+                foodCnt.addAll(subscribeWeek.getWeeklyFoods()
+                        .stream()
+                        .map(WeeklyFood::getFood)
+                        .map(food -> NextWeekFood.builder()
+                                .foodId(food.getId())
+                                .foodName(food.getName())
+                                .foodCnt(subscribeMap.get(subscribeId))
+                                .build())
+                        .toList());
+            }
         }
 
         //최종에 넣기
         return NextWeekQuantityResponse
-            .builder()
-            .foods(foodCnt)
-            .build();
+                .builder()
+                .foods(foodCnt)
+                .build();
     }
 
     @Override
@@ -221,17 +227,17 @@ public class SellerServiceImpl implements SellerService {
         //해당 구독 상품의 가격과 총 가격 가져오기
         for (Object[] object : todaySubscribeSales) {
             products.add(ProductDto
-                .builder()
-                .price(((Number) object[1]).longValue())
-                .productCnt((int) object[0])
-                .productName((String) object[2])
-                .salesSum((int) object[0] * (int) object[1])
-                .build());
+                    .builder()
+                    .price(((Number) object[1]).longValue())
+                    .productCnt((int) object[0])
+                    .productName((String) object[2])
+                    .salesSum((int) object[0] * (int) object[1])
+                    .build());
         }
 
         //해당 가게의 오늘날짜의 일반 상품 주문 가져오기
         List<Order> orders = orderRepository.findByOrderTypeAndCreatedAtToday(OrderType.FOOD,
-            storeId);
+                storeId);
 
         //해당 주문의 상품을 돌면서 반찬의 가격 가져와서 개수 곱해가주고 넣기
         Map<String, Integer> map = new HashMap<>();
@@ -250,20 +256,20 @@ public class SellerServiceImpl implements SellerService {
         for (String foodName : map.keySet()) {
             Food food = foodRepository.findByName(foodName).orElseThrow(FoodNotFoundException::new);
             products.add(ProductDto
-                .builder()
-                .productName(foodName)
-                .productCnt(map.get(foodName))
-                .price(Long.parseLong(String.valueOf(food.getPrice())))
-                .salesSum(food.getPrice() * map.get(foodName))
-                .build());
+                    .builder()
+                    .productName(foodName)
+                    .productCnt(map.get(foodName))
+                    .price(Long.parseLong(String.valueOf(food.getPrice())))
+                    .salesSum(food.getPrice() * map.get(foodName))
+                    .build());
         }
 
         products.sort((o1, o2) -> o2.getSalesSum() - o1.getSalesSum());
 
         return TodaySalesResponse
-            .builder()
-            .products(products)
-            .build();
+                .builder()
+                .products(products)
+                .build();
     }
 
     @Override
@@ -273,11 +279,11 @@ public class SellerServiceImpl implements SellerService {
         int positiveCnt = reviewRepository.getCount(storeId, true);
         int negativeCnt = reviewRepository.getCount(storeId, false);
         return SellerReviewAllResponse
-            .builder()
-            .storeId(storeId)
-            .positiveCnt(positiveCnt)
-            .negativeCnt(negativeCnt)
-            .build();
+                .builder()
+                .storeId(storeId)
+                .positiveCnt(positiveCnt)
+                .negativeCnt(negativeCnt)
+                .build();
     }
 
     @Override
@@ -290,36 +296,36 @@ public class SellerServiceImpl implements SellerService {
             int positiveCnt = reviewRepository.getCount(storeId, true);
             int negativeCnt = reviewRepository.getCount(storeId, false);
             Store store = storeRepository.findById(storeId)
-                .orElseThrow(StoreNotFoundException::new);
+                    .orElseThrow(StoreNotFoundException::new);
             List<ReviewDetail> reviewDetails = new ArrayList<>();
             String aiSummery = store.getSummary();
 
             Pageable pageable = PageRequest.of(page - 1, 20);
             List<Review> reviews = reviewRepository.findReviewByStoreId(storeId, pageable)
-                .getContent();
+                    .getContent();
 
             for (Review review : reviews) {
                 String memberEmail = review.getOrderList().getOrder().getMember().getEmail();
                 String foodName = review.getOrderList().getFood().getName();
                 reviewDetails.add(ReviewDetail
-                    .builder()
-                    .img(review.getImg())
-                    .comment(review.getComment())
-                    .foodName(foodName)
-                    .memberEmail(memberEmail)
-                    .emotion(review.isEmotion())
-                    .build());
+                        .builder()
+                        .img(review.getImg())
+                        .comment(review.getComment())
+                        .foodName(foodName)
+                        .memberEmail(memberEmail)
+                        .emotion(review.isEmotion())
+                        .build());
             }
             return ReviewDetailResponse
-                .builder()
-                .reviews(reviewDetails)
-                .chart(ReviewChart
                     .builder()
-                    .positiveCnt(positiveCnt)
-                    .negativeCnt(negativeCnt)
-                    .aiSummary(aiSummery)
-                    .build())
-                .build();
+                    .reviews(reviewDetails)
+                    .chart(ReviewChart
+                            .builder()
+                            .positiveCnt(positiveCnt)
+                            .negativeCnt(negativeCnt)
+                            .aiSummary(aiSummery)
+                            .build())
+                    .build();
         } else {
             //상품일 때
             //차트 정보 가져오기
@@ -333,29 +339,29 @@ public class SellerServiceImpl implements SellerService {
 
             Pageable pageable = PageRequest.of(page - 1, 20);
             List<Review> reviews = reviewRepository.findReviewByStoreIdAndFoodId(storeId, foodId,
-                pageable).getContent();
+                    pageable).getContent();
 
             for (Review review : reviews) {
                 String memberEmail = review.getOrderList().getOrder().getMember().getEmail();
                 String foodName = review.getOrderList().getFood().getName();
                 reviewDetails.add(ReviewDetail
-                    .builder()
-                    .img(review.getImg())
-                    .foodName(foodName)
-                    .comment(review.getComment())
-                    .memberEmail(memberEmail)
-                    .build());
+                        .builder()
+                        .img(review.getImg())
+                        .foodName(foodName)
+                        .comment(review.getComment())
+                        .memberEmail(memberEmail)
+                        .build());
             }
             return ReviewDetailResponse
-                .builder()
-                .reviews(reviewDetails)
-                .chart(ReviewChart
                     .builder()
-                    .positiveCnt(positiveCnt)
-                    .negativeCnt(negativeCnt)
-                    .aiSummary(aiSummery)
-                    .build())
-                .build();
+                    .reviews(reviewDetails)
+                    .chart(ReviewChart
+                            .builder()
+                            .positiveCnt(positiveCnt)
+                            .negativeCnt(negativeCnt)
+                            .aiSummary(aiSummery)
+                            .build())
+                    .build();
         }
     }
 
@@ -368,13 +374,13 @@ public class SellerServiceImpl implements SellerService {
         List<Subscribe> storeSubscribes = subscribeRepository.findAllByStore_Id(storeId);
         for (Subscribe subscribe : storeSubscribes) {
             subscribes.add(SubscribeDetail
-                .builder()
-                .subscribeId(subscribe.getId())
-                .subscribeBeforePrice(subscribe.getBeforePrice())
-                .subscribeDescription(subscribe.getDescription())
-                .subscribeName(subscribe.getName())
-                .subscribePrice(subscribe.getPrice())
-                .build());
+                    .builder()
+                    .subscribeId(subscribe.getId())
+                    .subscribeBeforePrice(subscribe.getBeforePrice())
+                    .subscribeDescription(subscribe.getDescription())
+                    .subscribeName(subscribe.getName())
+                    .subscribePrice(subscribe.getPrice())
+                    .build());
         }
 
         //가게의 개별 반찬 가져오기
@@ -382,20 +388,20 @@ public class SellerServiceImpl implements SellerService {
         List<Food> storeFoods = foodRepository.findAllByStore_Id(storeId);
         for (Food food : storeFoods) {
             foods.add(FoodDetail
-                .builder()
-                .foodId(food.getId())
-                .foodDescription(food.getDescription())
-                .foodImg(food.getImg())
-                .foodPrice(food.getPrice())
-                .foodName(food.getName())
-                .build());
+                    .builder()
+                    .foodId(food.getId())
+                    .foodDescription(food.getDescription())
+                    .foodImg(food.getImg())
+                    .foodPrice(food.getPrice())
+                    .foodName(food.getName())
+                    .build());
         }
 
         return SellerMenuResponse
-            .builder()
-            .subscribes(subscribes)
-            .foods(foods)
-            .build();
+                .builder()
+                .subscribes(subscribes)
+                .foods(foods)
+                .build();
     }
 
     @Override
@@ -404,22 +410,22 @@ public class SellerServiceImpl implements SellerService {
 
         List<FoodDetailDto> foods = new ArrayList<>();
         foods.add(FoodDetailDto
-            .builder()
-            .foodId(-1L)
-            .foodName("전체")
-            .build());
+                .builder()
+                .foodId(-1L)
+                .foodName("전체")
+                .build());
         List<Food> foodList = foodRepository.findAllByStore_Id(storeId);
         for (Food food : foodList) {
             foods.add(FoodDetailDto
-                .builder()
-                .foodId(food.getId())
-                .foodName(food.getName())
-                .build());
+                    .builder()
+                    .foodId(food.getId())
+                    .foodName(food.getName())
+                    .build());
         }
         return FoodListResponse
-            .builder()
-            .foods(foods)
-            .build();
+                .builder()
+                .foods(foods)
+                .build();
     }
 
     LocalDate getnextMonday() {
@@ -431,11 +437,11 @@ public class SellerServiceImpl implements SellerService {
     private String createSummaryWithAI(String content) {
         // ChatModel을 통해 AI 호출 및 요약 생성
         String summary = chatClient
-            .prompt()
-            .system("반찬을 시켜먹는 사람들이 쓴 리뷰 입니다. 이 리뷰들을 간단하게 한줄로 요약 해주세요")
-            .user(content)
-            .call()
-            .content();
+                .prompt()
+                .system("반찬을 시켜먹는 사람들이 쓴 리뷰 입니다. 이 리뷰들을 간단하게 한줄로 요약 해주세요")
+                .user(content)
+                .call()
+                .content();
         if (summary.isEmpty()) {
             throw new SummeryNotFoundException();
         }
@@ -471,15 +477,15 @@ public class SellerServiceImpl implements SellerService {
 
         // 리뷰 코멘트 결함
         String reviewContent = reviews.stream()
-            .map(Review::getComment)
-            .collect(Collectors.joining(" "));
+                .map(Review::getComment)
+                .collect(Collectors.joining(" "));
 
         // AI 요약 생성
         String summary = createSummaryWithAI(reviewContent);
 
         // Store 엔티티의 summary 필드에 업데이트
         Store store = storeRepository.findById(storeId)
-            .orElseThrow(StoreNotFoundException::new);
+                .orElseThrow(StoreNotFoundException::new);
         store.updateSummary(summary);
         storeRepository.save(store);
     }
@@ -493,15 +499,15 @@ public class SellerServiceImpl implements SellerService {
 
         // 리뷰 코멘트 결합
         String reviewContent = reviews.stream()
-            .map(Review::getComment)
-            .collect(Collectors.joining(" "));
+                .map(Review::getComment)
+                .collect(Collectors.joining(" "));
 
         // AI 요약 생성
         String summary = createSummaryWithAI(reviewContent);
 
         // Food 엔티티의 summary 필드에 업데이트
         Food food = foodRepository.findById(foodId)
-            .orElseThrow(FoodNotFoundException::new);
+                .orElseThrow(FoodNotFoundException::new);
         food.updateSummary(summary);
         foodRepository.save(food);
     }
