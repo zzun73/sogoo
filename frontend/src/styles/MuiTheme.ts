555f0bb4ed449a114e3a42a0c3d14e8b4337e762
서규범 <seo980916@gmail.com>
2024-11-12 10:37:46 +0900

    feat: mui 커스텀 theme 설정

---
+++ frontend/src/styles/MuiTheme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6F4F3A",
+    },
+    secondary: {
+      main: "#D9BCA5",
+    },
+  },
+  typography: {
+    fontFamily: "KoPubWorld_Dotum_Medium",
+  },
+});
