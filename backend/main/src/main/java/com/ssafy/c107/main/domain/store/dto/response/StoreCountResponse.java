4a7706e832a9e932207552bd8a21264aa7fb1a63
최승현 <csh7099@naver.com>
2024-11-12 10:36:07 +0900

    feat : 가게 개수 dto 생성

---
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/store/dto/response/StoreCountResponse.java
@@ -0,0 +1,11 @@
+package com.ssafy.c107.main.domain.store.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StoreCountResponse {
+
+    private int storeCount;
+}
