575878db20d8576d69d6237933781bee05696c7a
박동민 <pmin1616@naver.com>
2024-11-14 14:42:52 +0900

    feat: 인당 매장 등록 가능 수를 3개로 제한

--- frontend/src/components/specific/Seller/MenuComponents/ChoiceStore.tsx
+++ frontend/src/components/specific/Seller/MenuComponents/ChoiceStore.tsx
@@ -5,6 +5,7 @@ import RegisterStoreModal from "./RegisterStoreModal";
 import { useGetMyStores } from "../../../../queries/queries";
 import useRootStore from "../../../../stores";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 interface StoreInfo {
   storeId: number;
@@ -16,10 +17,6 @@ const ChoiceStore = () => {
   const [openStoreModal, setOpenStoreModal] = useState(false);
 
   const navigate = useNavigate();
-
-  const handleStoreModalOpen = () => {
-    setOpenStoreModal(true);
-  };
 
   const handleStoreModalClose = () => {
     setOpenStoreModal(false);
@@ -45,29 +42,34 @@ const ChoiceStore = () => {
     navigate(-1);
   };
 
+  const handleStoreModalOpen = () => {
+    if (stores && stores.length >= 3) {
+      toast.error("1인당 가게는 최대 3개까지만 등록할 수 있습니다.");
+      return;
+    }
+    setOpenStoreModal(true);
+  };
   return (
     <div className="min-w-[1100px] flex justify-between items-center py-5">
       <div className="flex space-x-2">
         {stores && stores.length > 0 ? (
-          <div className="max-w-[850px] overflow-x-scroll whitespace-nowrap space-x-1">
-            {stores.map((store) => (
-              <Button
-                key={store.storeId}
-                variant={
-                  selectedStoreId === store.storeId ? "contained" : "outlined"
-                }
-                sx={{
-                  minWidth: "100px",
-                  minHeight: "50px",
-                }}
-                onClick={() => handleStoreClick(store.storeId)}
-              >
-                <Typography sx={{ fontSize: "18px", margin: 0 }}>
-                  {store.storeName}
-                </Typography>
-              </Button>
-            ))}
-          </div>
+          stores.map((store) => (
+            <Button
+              key={store.storeId}
+              variant={
+                selectedStoreId === store.storeId ? "contained" : "outlined"
+              }
+              sx={{
+                minWidth: "100px",
+                minHeight: "50px",
+              }}
+              onClick={() => handleStoreClick(store.storeId)}
+            >
+              <Typography sx={{ fontSize: "18px", margin: 0 }}>
+                {store.storeName}
+              </Typography>
+            </Button>
+          ))
         ) : (
           <></>
         )}
