3695871de51a837de2cdff9f01fc97c06cade4db
zzun73 <736300@naver.com>
2024-11-14 14:00:09 +0900

    feat: 인가 경로 추가

--- backend/main/src/main/java/com/ssafy/c107/main/common/security/SecurityConfig.java
+++ backend/main/src/main/java/com/ssafy/c107/main/common/security/SecurityConfig.java
@@ -95,7 +95,9 @@ public class SecurityConfig {
                 .requestMatchers("/health-check", "/member/sign-up", "/member/login",
                     "/member/logout", "/member/email-check", "/member/seller-check",
                     "/member/reissue", "/elastic/search", "/store/count", "/store",
-                    "/review/buyer/count/**", "/review/buyer/food/count/**")               //인가부분
+                    "/review/buyer/count/**", "/review/buyer/food/count/**",
+                        "store/count", "/store**"
+                        )               //인가부분
                 .permitAll()
                 .anyRequest().authenticated());
 
