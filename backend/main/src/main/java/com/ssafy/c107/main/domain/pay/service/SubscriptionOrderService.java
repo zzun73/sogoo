992a687afac61baf2cee16d5474e58b01fac9661
최승현 <csh7099@naver.com>
2024-11-15 14:52:18 +0900

    feat : 다음주 반찬 리스트 이메일 전송 로직 생성

--- backend/main/src/main/java/com/ssafy/c107/main/domain/pay/service/SubscriptionOrderService.java
+++ backend/main/src/main/java/com/ssafy/c107/main/domain/pay/service/SubscriptionOrderService.java
@@ -1,6 +1,7 @@
 package com.ssafy.c107.main.domain.pay.service;
 
 import com.ssafy.c107.main.common.entity.WeeklyFood;
+import com.ssafy.c107.main.domain.food.entity.Food;
 import com.ssafy.c107.main.domain.food.repository.WeeklyFoodRepository;
 import com.ssafy.c107.main.domain.members.entity.Member;
 import com.ssafy.c107.main.domain.order.entity.Order;
@@ -12,6 +13,7 @@ import com.ssafy.c107.main.domain.store.entity.Store;
 import com.ssafy.c107.main.domain.subscribe.entity.MemberSubscribe;
 import com.ssafy.c107.main.domain.subscribe.entity.SubscribeWeek;
 import com.ssafy.c107.main.domain.subscribe.repository.SubscribeWeekRepository;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -23,6 +25,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SubscriptionOrderService {
+
     private final OrderRepository orderRepository;
     private final OrderListRepository orderListRepository;
     private final SubscribeWeekRepository subscribeWeekRepository;
@@ -36,20 +39,21 @@ public class SubscriptionOrderService {
 
         // 구독 상품에 해당하는 현재 날짜와 일치하는 주차별 목록을 조회
         List<SubscribeWeek> subscribeWeeks = subscribeWeekRepository.findBySubscribeIdAndStartDate(
-                memberSubscribe.getSubscribe().getId(), LocalDate.now());
+            memberSubscribe.getSubscribe().getId(), LocalDate.now());
 
         // 주차별 반찬이 있는 경우에만 주문을 생성
         if (!subscribeWeeks.isEmpty()) {
             // 새로운 주문 생성
             Order order = Order.builder()
-                    .orderType(OrderType.SUBSCRIBE)
-                    .member(member)
-                    .store(store)
-                    .build();
+                .orderType(OrderType.SUBSCRIBE)
+                .member(member)
+                .store(store)
+                .build();
             orderRepository.save(order);
 
             for (SubscribeWeek subscribeWeek : subscribeWeeks) {
-                List<WeeklyFood> weeklyFoods = weeklyFoodRepository.findBySubscribeWeekId(subscribeWeek.getId());
+                List<WeeklyFood> weeklyFoods = weeklyFoodRepository.findBySubscribeWeekId(
+                    subscribeWeek.getId());
 
                 for (WeeklyFood weeklyFood : weeklyFoods) {
                     int foodPrice = weeklyFood.getFood().getPrice();
@@ -57,17 +61,42 @@ public class SubscriptionOrderService {
 
                     // 주문 목록 생성
                     OrderList orderList = OrderList.builder()
-                            .count(1) // 기본 수량을 1
-                            .price(foodPrice)
-                            .order(order)
-                            .food(weeklyFood.getFood())
-                            .build();
+                        .count(1) // 기본 수량을 1
+                        .price(foodPrice)
+                        .order(order)
+                        .food(weeklyFood.getFood())
+                        .build();
                     orderListRepository.save(orderList);
                 }
             }
 
             // 총 주문 금액 업데이트
             order.updateTotalPrice(totalPrice);
+        }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void sendEmail(MemberSubscribe memberSubscribe) {
+        Member member = memberSubscribe.getMember();
+        Store store = memberSubscribe.getSubscribe().getStore();
+
+        List<Food> foods = new ArrayList<>();
+
+        // 구독 상품에 해당하는 현재 날짜와 일치하는 주차별 목록을 조회
+        List<SubscribeWeek> subscribeWeeks = subscribeWeekRepository.findBySubscribeIdAndStartDate(
+            memberSubscribe.getSubscribe().getId(), LocalDate.now().plusDays(7));
+
+        // 주차별 반찬이 있는 경우에만 주문을 생성
+        if (!subscribeWeeks.isEmpty()) {
+
+            for (SubscribeWeek subscribeWeek : subscribeWeeks) {
+                List<WeeklyFood> weeklyFoods = weeklyFoodRepository.findBySubscribeWeekId(
+                    subscribeWeek.getId());
+
+                for (WeeklyFood weeklyFood : weeklyFoods) {
+                    foods.add(weeklyFood.getFood());
+                }
+            }
         }
     }
 }
