c364a1c26f7e4548f76549a925709c459a5e52a8
서규범 <seo980916@gmail.com>
2024-11-17 20:30:24 +0900

    fix: Footer 오타 수정

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-auto w-full h-14 bg-black text-white flex justify-center items-center z-10">
-      <div>Copyrights Sogoo.com. All Rights Reserved</div>
+      <div>Copyrights Sogoo.kr. All Rights Reserved</div>
     </footer>
   );
 };
