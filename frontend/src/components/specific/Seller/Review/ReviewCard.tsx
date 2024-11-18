588d828dfad873573444be7a1e5cf5091b3c3b53
박동민 <pmin1616@naver.com>
2024-11-18 20:36:26 +0900

    feat: 판매자 리뷰 상세 페이지에서 긍/부정 결과 표시를 각 card의 우측 끝에서 표시되도록 수정

--- frontend/src/components/specific/Seller/Review/ReviewCard.tsx
+++ frontend/src/components/specific/Seller/Review/ReviewCard.tsx
@@ -15,21 +15,21 @@ const ReviewCard = ({ review }: SellerReviewProps) => {
 
   console.log(emotion);
   return (
-    <Card className="flex flex-row gap-x-3 w-full mb-5 border-2 border-slate-200 p-5">
-      <img src={img} className="w-24 h-24" />
-      <div className="w-full flex justify-between items-center ms-5">
-        <div className="w-11/12 flex flex-col justify-evenly items-start ms-5">
-          <p>{memberEmail}</p>
-          <p className="font-bold">{foodName}</p>
-          <p className="text-sm">{comment}</p>
+    <Card className="flex flex-row gap-x-3 w-full mb-5 border-2 border-slate-200">
+      <div className="flex w-full p-5">
+        <img src={img} className="w-24 h-24" />
+        <div className="w-full flex justify-between items-center ms-5">
+          <div className="w-11/12 flex flex-col justify-evenly items-start ms-5">
+            <p>{memberEmail}</p>
+            <p className="font-bold">{foodName}</p>
+            <p className="text-sm">{comment}</p>
+          </div>
         </div>
-        <div className="w-1/12 flex justify-center items-center">
-          <div
-            className={`w-10 h-10 rounded-full ${
-              emotion ? "bg-green-700" : "bg-red-700"
-            }`}
-          ></div>
-        </div>
+      </div>
+      <div className="w-1/12 h-full flex justify-end items-center">
+        <div
+          className={`w-3 h-full ${emotion ? "bg-green-500" : "bg-red-500"}`}
+        ></div>
       </div>
     </Card>
   );
